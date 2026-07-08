f68e58f272c11f6d03d3d90c03b1a0c539faa315
Luka0752 <lukazhang0752@gmail.com>
2026-07-08 16:14:31 +0800

    fix: restore routing.ts locale list

--- routing.ts
+++ routing.ts
@@ -1,4 +1,6 @@
-export const routing = {
+import { defineRouting } from 'next-intl/routing';
+
+export const routing = defineRouting({
   locales: ['en', 'zh', 'zh-TW', 'es', 'fr', 'de', 'ja', 'ko', 'pt', 'ar'],
-  defaultLocale: 'en'
-};
+  defaultLocale: 'en',
+});
